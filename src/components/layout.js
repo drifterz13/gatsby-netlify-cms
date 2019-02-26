14c8bb0b42d8fdba1c3d20a07612b29dcb2c1769
Totsawat Manatsatien <mixz@Totsawats-MacBook-Pro.local>
2019-02-27 00:53:40 +0700

    transform md & typography

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -18,10 +17,9 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: `0 auto`,
+            margin: `3rem auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
---
+++ src/utils/typography.js
@@ -0,0 +1,18 @@
+import Typography from "typography"
+import funstonTheme from "typography-theme-funston"
+
+funstonTheme.headerFontFamily = ["Patua One", "sans-serif"]
+funstonTheme.bodyFontFamily = ["Source Sans Pro", "sans-serif"]
+funstonTheme.googleFonts = [
+  {
+    name: "Patua One",
+    styles: ["400", "700"],
+  },
+  {
+    name: "Source Sans Pro",
+    styles: ["400", "700"],
+  },
+]
+const typography = new Typography(funstonTheme)
+
+export default typography
